9365b814a698e072b0731875d1874efe63dbb77a
5Suminsss2 <1012ksm@naver.com>
2019-01-28 21:36:12 +0900

    2-7

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 import express from "express";
 //const express = require('express');
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => 
     console.log(`Listening on: http://localhost:${PORT}`);
-/*
-function handleHome(req, res) {
-    //console.log(req);
-    res.send("Hi from Home!");
 
-}*/
 
 const handleHome = (req, res) => res.send("Hi from ddd!");
 
 const handleProfile = (req, res) => res.send("U are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("I'm broken");
-    next();
-}
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(helmet());
 
-app.use(betweenHome);
+app.use(morgan("dev"));
+/* middleware가 중간에 연결 끊어주는 예시 
+const middleware = (req, res, next) => {
+    res.send("not happening");
+} 
+
+app.get("/", middleware, handleHome); */
 
 app.get("/", handleHome);
 
