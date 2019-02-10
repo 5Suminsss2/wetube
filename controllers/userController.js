92b34283a8fc4c1ca64d78762f681fd0b73ab9c0
5Suminsss2 <1012ksm@naver.com>
2019-02-10 22:40:46 +0900

    2-23

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,22 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Search" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+    res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+    const {
+        body: { name, email, password, password2}
+    } = req;
+    if(password !== password2) {
+        res.status(400);
+    } else{
+        // 할 것: 사용자등록 , 사용자 로그인
+        res.redirect(routes.home);
+    }
+    res.render("join", { pageTitle: "Join"});
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
