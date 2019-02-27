461c5078fc761b659ac43bd07162e5bb4eb4dc7b
5Suminsss2 <1012ksm@naver.com>
2019-02-27 23:42:54 +0900

    5-1

--- middlewares.js
+++ middlewares.js
@@ -4,14 +4,13 @@ import routes from "./routes";
 const multerdVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1
+  };
+  next();
 };
-
 
 export const uploadVideo = multerdVideo.single("videoFile");
