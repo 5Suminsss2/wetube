ab10ecfe9c3a92159b19b927423471433ca8e3ec
5Suminsss2 <1012ksm@naver.com>
2019-03-06 17:16:35 +0900

    6-3

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
@@ -5,33 +6,35 @@ export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
     res.status(400);
+    res.render("join", { pageTitle: "Join" });
   } else {
-    // 할 것: 사용자등록 , 사용자 로그인
     try {
       const user = await User({
         name,
         email
       });
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
-    res.redirect(routes.home);
   }
-  res.render("join", { pageTitle: "Join" });
 };
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
   res.redirect(routes.home);
